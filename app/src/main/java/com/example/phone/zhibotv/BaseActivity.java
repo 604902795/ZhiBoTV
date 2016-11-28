33e58d8e43854bc40ae85566615ee7e6f7bf2f72
604902795 <604902795@qq.com>
2016-11-28 10:06:29 +0800

    ludi

--- app/src/main/java/com/example/phone/zhibotv/BaseActivity.java
+++ app/src/main/java/com/example/phone/zhibotv/BaseActivity.java
@@ -1,5 +1,7 @@
 package com.example.phone.zhibotv;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -7,5 +9,8 @@ import android.view.View;
  * Created by Administrator on 2016-11-26.
  */
 public class BaseActivity extends AppCompatActivity {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
